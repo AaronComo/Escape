6b97ab8692a34666224fe4444f392826d2ea2129
AaronComo <2691839030@qq.com>
2024-03-31 23:07:39 +0800

    update

--- ServerBackend/clean.py
+++
@@ -1,4 +0,0 @@
-import os
-
-path = './static/upload'
-print(os.system(f'rm -f {path}/*'))
--- ServerBackend/test.py
+++
@@ -1,4 +0,0 @@
-filename = "Aaron_ss_123"
-print(filename.find("_"))
-print(filename[filename.find("_") + 1:])
-print(filename.find("11"))
